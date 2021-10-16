3da8b5136b74389b5b39b70b2cdb1bf14e022216
Vinny D <thevinnycast@gmail.com>
2021-10-16 10:54:07 -0700

    basic card implemented

---
+++ src/Card.java
@@ -0,0 +1,42 @@
+public class Card
+{
+    private int value; // [0,12]
+    private boolean color; // F=Red, T=Black
+    private boolean suit; // Red- F=Hearts, T=Diamonds; Black- F=Spades, T=Clubs
+
+    public static final String[] CARDS = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+            "Jack", "Queen", "King"};
+
+    public Card(int value, boolean color, boolean suit)
+    {
+        this.value = value;
+        this.color = color;
+        this.suit = suit;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public String toString()
+    {
+        String str = CARDS[value] + " of ";
+
+        if (color)
+        {
+            if (suit)
+                str += "Clubs";
+            else
+                str += "Spades";
+        }
+        else
+        {
+            if (suit)
+                str += "Diamonds";
+            else
+                str += "Hearts";
+        }
+        return str;
+    }
+}
