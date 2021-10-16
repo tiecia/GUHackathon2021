e404f11d3507e18652cbc12675a3285cd485467d
DrewLaptop <drew@bnimble.org>
2021-10-16 15:19:43 -0700

    Finaliziation of drew server stuff

--- src/Dealer.java
+++ src/Dealer.java
@@ -5,14 +5,28 @@ public class Dealer {
         deck = new Deck();
     }
 
+    /**
+     * takes a card from the deck and gives
+     * it away like this is a communist
+     * society. do you think computer
+     * chips have governments
+     * @return
+     */
     public Card dealCard() {
         return deck.draw();
     }
 
+    /**
+     * gives a new deck so cardss
+     * dont need to be returned
+     */
     public void newRound() {
         deck.newDeck();
     }
 
+    /**
+     * shuffles the current deck
+     */
     public void shuffleDeck() {
         deck.shuffle();
     }
