4acd578b961f7a52d3bb732182aa46a33de220b4
DrewLaptop <drew@bnimble.org>
2021-10-16 15:05:18 -0700

    Working on client/server managability

--- src/BoardManager.java
+++ src/BoardManager.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class BoardManager {
+
+    private ArrayList<Card> currentBoard;
+
+    public BoardManager() {
+        newRound();
+    }
+
+    public void dealCards(Dealer dealer, int num) {
+        dealer.dealCard();
+        for(int count = 0; count < num; ++count) {
+            currentBoard.add(dealer.dealCard());
+        }
+    }
+
+    public void newRound() {
+        currentBoard = new ArrayList<Card>();
+    }
+
 }
--- src/Dealer.java
+++ src/Dealer.java
@@ -1,3 +1,20 @@
 public class Dealer {
+    private Deck deck;
+
+    public Dealer() {
+        deck = new Deck();
+    }
+
+    public Card dealCard() {
+        return deck.draw();
+    }
+
+    public void newRound() {
+        deck.newDeck();
+    }
+
+    public void shuffleDeck() {
+        deck.shuffle();
+    }
 
 }
